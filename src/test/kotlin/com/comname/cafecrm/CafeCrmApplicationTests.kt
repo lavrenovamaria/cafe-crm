164698c97db9ddd73b7c7641cf9e707564770d1b
Anton Bogomazov <anton.p.bogomazov@gmail.com>
2022-06-07 14:31:04 +0300

    Init project

---
+++ src/test/kotlin/com/comname/cafecrm/CafeCrmApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.comname.cafecrm
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CafeCrmApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
