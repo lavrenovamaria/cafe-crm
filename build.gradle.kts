103c6c5a94e447412687b6011e76b5429f582b07
Anton Bogomazov <anton.p.bogomazov@gmail.com>
2022-06-10 22:58:31 +0300

    Add OpenAPI UI

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,6 +27,8 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.liquibase:liquibase-core")
 
+    implementation("org.springdoc:springdoc-openapi-ui:1.6.8")
+
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
